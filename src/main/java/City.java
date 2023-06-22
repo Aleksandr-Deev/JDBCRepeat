4c86b60ea50f79c29a1facd2192f3b37e7175571
Aleksandr Deev <geshtait@mail.ru>
2023-06-22 19:00:14 +0500

    Сделал ДЗ JDBC (Повторение)

---
+++ src/main/java/City.java
@@ -0,0 +1,28 @@
+public class City {
+
+    private int cityId;
+    private String cityName;
+
+    public City() {};
+
+    public City(int cityId, String cityName) {
+        this.cityId = cityId;
+        this.cityName = cityName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCtyId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
